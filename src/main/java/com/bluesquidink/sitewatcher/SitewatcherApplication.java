1ea15e18050dc11f89efe5a45be9831b2344d53a
sobryan <stobryan@gmail.com>
2020-11-15 13:35:47 -0600

    initial

---
+++ src/main/java/com/bluesquidink/sitewatcher/SitewatcherApplication.java
@@ -0,0 +1,13 @@
+package com.bluesquidink.sitewatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SitewatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SitewatcherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bluesquidink/sitewatcher/SitewatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluesquidink.sitewatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SitewatcherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
